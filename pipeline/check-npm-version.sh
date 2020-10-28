35608800401f5ec6b36511ee960ac45920db858f
Luke 10X <spam@luke10x.dev>
2020-10-28 20:34:52 +0000

    💚 Checks latest published version in npm
    Currently it uses different package name

---
+++ pipeline/check-npm-version.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+published_version=$(npm view p10p10p10 version)
+this_version=$(node -p -e "require('./package.json').version")
+echo "🌐 latest published version: $published_version"
+echo "🚧 package version of this build: $this_version"
+
+if [ "$published_version" = "$this_version" ]; then
+    echo "🦘 skip NPM Publish, because this version is published"
+    exit 1
+else
+    echo "🐱‍🏍 Update NPM, yes please!"
+fi
